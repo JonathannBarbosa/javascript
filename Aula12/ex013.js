39c6d93c1581d3d22170adb010c233fb5eb1efcb
JonathanBarbosa <jonathanbazevedo@hotmail.com>
2023-04-03 11:03:35 -0300

    Initial commit

---
+++ Aula12/ex013.js
@@ -0,0 +1,54 @@
+ var agora = new Date()
+var diaSem = agora.getDay()
+
+var diaSem = 5
+switch(diaSem){
+  case 0:
+    console.log('Domingo')
+    break
+
+  case 1:
+    console.log('Segunda-feira')
+    break
+
+  case 2:
+    console.log('Terça-feira')
+    break
+
+  case 3:
+    console.log('Quarta-feira')
+    break
+
+  case 4:
+    console.log('Quinta-feira')
+    break
+
+  case 5:
+    console.log('Sexta-feira')
+    break
+
+  case 6:
+    console.log('Sábado')
+    break
+
+    default:
+      console.log('[ERRO] Dia da semana inválido')
+      break
+}
+
+var cor = 1
+var cor = 2
+var cor = 3 
+
+switch(1){ // outro exemplo criado
+  case 1:
+    console.log('Vermelho')
+    break
+  case 2:
+    console.log('Verde')
+  case 3:
+    console.log('Preto')
+}
+
+
+
